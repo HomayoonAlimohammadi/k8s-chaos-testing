e8778baa12f0f5dbbb0de0a7676ae6f922e9bda6
Homayoon Alimohammadi <homayoonalimohammadi@gmail.com>
2025-09-10 10:43:40 +0400

    fix: Update pod-cpu-hog-exec
    Signed-off-by: Homayoon Alimohammadi <homayoonalimohammadi@gmail.com>

---
+++ litmus/tests/kubernetes/pod-cpu-hog-exec/verify.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -euo pipefail
+
+TRIES=100
+SLEEP=5
+DIR_NAME=$(basename "$(pwd)")
+
+for ((i=1; i<=TRIES; i++)); do
+  echo "[$i/$TRIES] Verifying..."
+  if python3 ../util/check_result.py --name nginx-chaos-$DIR_NAME --namespace litmus; then
+    echo "Success!"
+    exit 0
+  fi
+  if (( i < TRIES )); then
+    echo "Retrying in $SLEEP seconds..."
+    sleep "$SLEEP"
+  fi
+done
+
+echo "Failed after $TRIES attempts."
+exit 1
